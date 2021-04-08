7bf77f650988febf6ec39a36e3607811f09ad9c5
Benny Lam(work) <bennylam6@gmail.com>
2021-04-08 17:15:07 -0400

    Add initial types and interfaces

---
+++ src/models/data_models/IConfiguration.ts
@@ -0,0 +1,31 @@
+export interface IConfiguration {
+	googlePlaces: GooglePlaces;
+	yelp: Yelp;
+	openCage: OpenCage;
+}
+
+export interface Yelp {
+	apiKey: string;
+	clientID: string;
+}
+
+export interface GooglePlaces {
+	apiKey: string;
+}
+
+export interface OpenCage {
+	apiKey: string;
+}
+
+export const defaultConfig: IConfiguration = {
+	googlePlaces: {
+		apiKey: ''
+	},
+	yelp: {
+		apiKey: '',
+		clientID: ''
+	},
+	openCage: {
+		apiKey: ''
+	}
+}
