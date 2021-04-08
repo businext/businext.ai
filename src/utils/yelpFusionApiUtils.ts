e040b9e20b986ba6a4ebf74c0a57fa57b0cb2cf5
Benny Lam(work) <bennylam6@gmail.com>
2021-04-08 18:41:19 -0400

    Fetch images from yelp

--- src/utils/yelpFusionApiUtils.ts
+++ src/utils/yelpFusionApiUtils.ts
@@ -1,6 +1,7 @@
 import yelp from 'yelp-fusion';
 import { YelpClient } from 'yelp-fusion';
 import { IConfiguration } from '../models/data_models/IConfiguration.js';
+import { Geocode } from '../models/data_models/types.js';
 
 export class YelpFusionApiUtils {
 	private yelpClient?: YelpClient;
@@ -11,5 +12,26 @@ export class YelpFusionApiUtils {
 		const apiKey = this.config.yelp.apiKey;
 		this.yelpClient = yelp.client(apiKey);
 		return this;
+	}
+
+	public async getImages(name: string, geocode: Geocode): Promise<string[]> {
+		// calls the api twice to first get the id of the business and then to actually retrieve the photos...
+		const response = await this.yelpClient.search({
+			term: name,
+			latitude: geocode.lat,
+			longitude: geocode.lng,
+		});
+
+		const id = response?.jsonBody?.businesses?.[0].id;
+		const images: string[] = [];
+		if (id) {
+			await this.yelpClient.business(id).then((result) => {
+				if (result?.jsonBody?.photos) {
+					images.push(...result.jsonBody.photos);
+				}
+			});
+		}
+
+		return images;
 	}
 }
