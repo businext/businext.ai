76d3ce4b213257b199924e891d305811bdd7320a
Benny Lam(work) <bennylam6@gmail.com>
2021-04-08 17:21:06 -0400

    Add yelp utils

---
+++ src/utils/yelpFusionApiUtils.ts
@@ -0,0 +1,17 @@
+
+import yelp from 'yelp-fusion'
+import {YelpClient} from 'yelp-fusion'
+import {IConfiguration} from '../models/data_models/IConfiguration.js'
+
+
+export class YelpFusionApiUtils {
+    private yelpClient?: YelpClient;
+
+    constructor(private config: IConfiguration) {}
+
+    public async init(): Promise<YelpFusionApiUtils> {
+        const apiKey = this.config.yelp.apiKey;
+        this.yelpClient = yelp.client(apiKey);
+        return this;
+    }
+}
