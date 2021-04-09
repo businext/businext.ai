e6bb3c83e0af21cf5fefed44bd5cf2bb818030be
Benny Lam(work) <bennylam6@gmail.com>
2021-04-09 16:39:52 -0400

    Simplify return object

--- src/utils/openCageApiUtils.ts
+++ src/utils/openCageApiUtils.ts
@@ -13,20 +13,27 @@ export class OpenCageApiUtils {
 	}
 
 	private reformatGeocodeResult(response: any): Address {
-		const output: Address = {};
-
 		// this seems like a very dirty way to reformat and filter the return object of the geocode
-		if (response?.formatted) output.formattedAddress = response.formatted;
-		const locationComponents = response?.components;
-		if (locationComponents) {
-			if (locationComponents.city) output.city = locationComponents.city;
-			if (locationComponents.state_code) output.stateCode = locationComponents.state_code;
-			if (locationComponents.postcode) output.postalCode = locationComponents.postcode;
-			if (locationComponents.country_code) output.countryCode = locationComponents.country_code;
-		}
-		if (response?.geometry) output.geocode = response.geometry;
+		// if (response?.formatted) output.formattedAddress = response.formatted;
+		// const locationComponents = response?.components;
+		// if (locationComponents) {
+		// 	if (locationComponents.city) output.city = locationComponents.city;
+		// 	if (locationComponents.state_code) output.stateCode = locationComponents.state_code;
+		// 	if (locationComponents.postcode) output.postalCode = locationComponents.postcode;
+		// 	if (locationComponents.country_code) output.countryCode = locationComponents.country_code;
+		// }
+		// if (response?.geometry) output.geocode = response.geometry;
 
-		return output;
+		// return output;
+		const { components, formatted, geometry } = response;
+		return {
+			formattedAddress: formatted,
+			city: components?.city,
+			stateCode: components?.state_code,
+			postalCode: components?.postcode,
+			countryCode: components?.country_code,
+			geocode: geometry,
+		};
 	}
 
 	public async addressLookup(address: string): Promise<Address | null> {
