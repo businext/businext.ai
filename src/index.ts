86a23a09f0d7396f1134c27ac96ef9d17bf3ee8e
Tim Zwart <timrzwart@gmail.com>
2021-04-12 16:18:12 -0400

    Make graphql server port customizeable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const server_port_default: number = 4000;
+const server_port: number = parseInt(process.env.ENDPOINT_PORT || '', 10) || server_port_default;
+
 const typeDefs = gql`
 	type Query {
 		dummy: String
@@ -13,6 +16,10 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
-});
+server
+	.listen({
+		port: server_port,
+	})
+	.then(({ url }) => {
+		console.log(`🚀  Server ready at ${url}`);
+	});
