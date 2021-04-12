e923b2965a9b3bf0d01c9ae37a4673d56e1b9c1e
Tim Zwart <timrzwart@gmail.com>
2021-04-12 17:52:07 -0400

    Replace IEvidenceCollection with Record

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,2 @@
 export { IData as Data, IExtracted as Extracted } from './data';
-export {
-	IEvidenceSource as EvidenceSource,
-	Evidence,
-	IEvidenceCollection as EvidenceCollection,
-	Insight,
-} from './insight';
+export { IEvidenceSource as EvidenceSource, Evidence, Insight } from './insight';
