673c51c9b312344ac4012e3ad29deaf24b436272
Benny Lam(work) <bennylam6@gmail.com>
2021-04-12 18:27:22 -0400

    Add bracket

--- src/utils/yelpFusionApiUtils.ts
+++ src/utils/yelpFusionApiUtils.ts
@@ -24,16 +24,18 @@ export class YelpFusionApiUtils {
 		});
 
 		const id = response?.jsonBody?.businesses?.[0].id;
-		const images: Array<IImage> = (
-			id && await this.yelpClient.business(id).then((result) => (
-				result.jsonBody?.photos?.map((photo) => (
-					<IImage>{
-						source: photo,
-						provider: EImageProvider.yelp,
-					}
-				)
-			)) || []
-		);
+		const images: Array<IImage> =
+			id &&
+			(await this.yelpClient.business(id).then(
+				(result) =>
+					result.jsonBody?.photos?.map(
+						(photo) =>
+							<IImage>{
+								source: photo,
+								provider: EImageProvider.yelp,
+							}
+					) || []
+			));
 
 		return images;
 	}
