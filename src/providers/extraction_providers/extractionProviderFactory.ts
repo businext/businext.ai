e18cafae7e0aa7b51444cb845ea1bdc10519c33a
Joshua Cheng <josh510cheng@gmail.com>
2021-04-13 10:49:08 -0400

    Adopt enum convention

--- src/providers/extraction_providers/extractionProviderFactory.ts
+++ src/providers/extraction_providers/extractionProviderFactory.ts
@@ -1,14 +1,20 @@
 import { ExtractionProvider } from './extractionProvider';
 import { MockExtractionProvider } from './mockExtractionProvider';
 
-interface ExtractionConfig {
-	extractionProviderName: 'mock';
+export const enum ExtractionProviderName {
+	mock = 'MOCK',
+}
+
+export interface ExtractionConfig {
+	extractionProviderName: string;
 }
 
 export const getExtractionProvider = (config: ExtractionConfig): ExtractionProvider => {
-	const { extractionProviderName } = config;
-	switch (extractionProviderName) {
-		case 'mock':
+	const { extractionProviderName: name } = config;
+	switch (name) {
+		case ExtractionProviderName.mock:
 			return new MockExtractionProvider();
+		default:
+			throw Error(`${name} is not a valid name for an interpretation provider`);
 	}
 };
