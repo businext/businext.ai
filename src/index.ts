54990922aead0fc15780cdeb5c17190b0185b531
Tim Zwart <timrzwart@gmail.com>
2021-04-14 17:45:22 -0400

    Use the GraphQL schema files for the endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { loadSchema } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 
 const server_port_default: number = 4000;
 const server_port: number = process.env.ENDPOINT_PORT
 	? parseInt(process.env.ENDPOINT_PORT)
 	: server_port_default;
 
-const typeDefs = gql`
-	type Query {
-		dummy: String
-	}
-`;
-const resolvers = {
-	Query: {
-		dummy: () => '',
-	},
-};
-const server = new ApolloServer({ typeDefs, resolvers });
-
-// The `listen` method launches a web server.
-server
-	.listen({
-		port: server_port,
-	})
-	.then(({ url }) => {
-		console.log(`🚀  Server ready at ${url}`);
+const main = async () => {
+	const schema = await loadSchema('./lib/graphql/schema/schema.graphql', {
+		loaders: [new GraphQLFileLoader()],
 	});
+	const resolvers = {
+		Query: {},
+	};
+	const server = new ApolloServer({ schema, resolvers });
+
+	// The `listen` method launches a web server.
+	server
+		.listen({
+			port: server_port,
+		})
+		.then(({ url }) => {
+			console.log(`🚀  Server ready at ${url}`);
+		});
+};
+main();
