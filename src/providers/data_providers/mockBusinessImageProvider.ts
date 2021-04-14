7727328e86c7574870d0ed890823acbe060d2f21
Benny Lam(work) <bennylam6@gmail.com>
2021-04-14 17:46:23 -0400

    Remove `Promise.resolve`

--- src/providers/data_providers/mockBusinessImageProvider.ts
+++ src/providers/data_providers/mockBusinessImageProvider.ts
@@ -4,7 +4,7 @@ import { Image, ImageProviderName } from '../../models/data_models/image';
 
 export class MockBusinessImageProvider extends BaseBusinessImageProvider {
 	public async getBusinessImages(businessInfo: BusinessInfoInput): Promise<Array<Image>> {
-		return Promise.resolve([
+		return [
 			{
 				source: 'https://live.staticflickr.com/5475/11104337814_0ff2f27b74_b.jpg',
 				provider: ImageProviderName.mock,
@@ -14,6 +14,6 @@ export class MockBusinessImageProvider extends BaseBusinessImageProvider {
 					'https://images.unsplash.com/photo-1501843508755-af0829d48618?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mzh8fHxlbnwwfHx8&w=1000&q=80',
 				provider: ImageProviderName.mock,
 			},
-		]);
+		];
 	}
 }
