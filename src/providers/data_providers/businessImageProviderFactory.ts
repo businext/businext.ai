6b4c3d58d78c3dd5832ddc39292a00dc9fac7a3d
Benny Lam(work) <bennylam6@gmail.com>
2021-04-14 12:48:34 -0400

    Add image provider factory

---
+++ src/providers/data_providers/businessImageProviderFactory.ts
@@ -0,0 +1,20 @@
+import { BusinessImageProvider } from './businessImageProvider';
+import { ImageProviderName } from '../../models/data_models/image';
+import { MockBusinessImageProvider } from './mockBusinessImageProvider';
+import { YelpBusinessImageProvider } from './yelpBusinessImageProvider';
+
+export interface ImageProviderConfig {
+	imageProviderName: string;
+}
+
+export const getImageProvider = async (config: ImageProviderConfig): Promise<BusinessImageProvider> => {
+	const { imageProviderName: name } = config;
+	switch (name) {
+		case ImageProviderName.mock:
+			return new MockBusinessImageProvider();
+		case ImageProviderName.yelp:
+			return new YelpBusinessImageProvider();
+		default:
+			throw Error(`${name} is not a valid name for an image provider`);
+	}
+};
