c5d0dbb2730acdf66b56e314a46a264f13daa369
Benny Lam(work) <bennylam6@gmail.com>
2021-04-14 17:45:09 -0400

    Update mock data provider to match extraction mock

--- src/providers/data_providers/mockBusinessImageProvider.ts
+++ src/providers/data_providers/mockBusinessImageProvider.ts
@@ -6,23 +6,12 @@ export class MockBusinessImageProvider extends BaseBusinessImageProvider {
 	public async getBusinessImages(businessInfo: BusinessInfoInput): Promise<Array<Image>> {
 		return Promise.resolve([
 			{
-				source: 'https://s3-media0.fl.yelpcdn.com/bphoto/AZllw6P3nvB0BCmsw5kMjQ/l.jpg',
+				source: 'https://live.staticflickr.com/5475/11104337814_0ff2f27b74_b.jpg',
 				provider: ImageProviderName.mock,
 			},
 			{
-				source: 'https://www.ubburger.com/wp-content/uploads/2016/01/locations_img.jpg',
-				provider: ImageProviderName.mock,
-			},
-			{
-				source: 'https://live.staticflickr.com/7295/10218344623_a6764810d4_b.jpg',
-				provider: ImageProviderName.mock,
-			},
-			{
-				source: 'https://s3-media0.fl.yelpcdn.com/bphoto/V50Cg1SnHT0qqsWSOmW8CA/l.jpg',
-				provider: ImageProviderName.mock,
-			},
-			{
-				source: 'https://pbs.twimg.com/media/DqsL2IXWwAArsrR.jpg',
+				source:
+					'https://images.unsplash.com/photo-1501843508755-af0829d48618?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mzh8fHxlbnwwfHx8&w=1000&q=80',
 				provider: ImageProviderName.mock,
 			},
 		]);
