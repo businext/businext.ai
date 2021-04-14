d3f5c2a3034c481c5bb86ca4a029608ee3fa3c92
Benny Lam <bennylam6@gmail.com>
2021-04-14 17:48:56 -0400

    Apply suggestions from code review
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>

--- src/providers/data_providers/businessImageProviderFactory.ts
+++ src/providers/data_providers/businessImageProviderFactory.ts
@@ -6,7 +6,7 @@ import { Image } from '../../models/data_models/image';
 import { BusinessInfoInput } from '../../models/data_models/types';
 
 export class BusinessImageProviderFactory {
-	private getImageProviderFromString(provider: string): BusinessImageProvider {
+	private getImageProvider(provider: string): BusinessImageProvider {
 		switch (provider) {
 			case ImageProviderName.mock:
 				return new MockBusinessImageProvider();
@@ -17,17 +17,17 @@ export class BusinessImageProviderFactory {
 		}
 	}
 
-	private getImageProvider(): Array<BusinessImageProvider> {
+	private getImageProviders(): Array<BusinessImageProvider> {
 		const imageProviders = process.env.IMAGE_PROVIDERS?.split(',').map((provider) => {
-			return this.getImageProviderFromString(provider.trim());
+			return this.getImageProvider(provider.trim().toUpperCase());
 		});
 		if (imageProviders) return imageProviders;
 		throw Error('IMAGE_PROVIDERS variable not set');
 	}
 
 	public async getImages(businessInfo: BusinessInfoInput): Promise<Array<Image>> {
-		const providers = this.getImageProvider();
-		return await Promise.all(
+		const providers = this.getImageProviders();
+		return Promise.all(
 			providers.map((provider) => provider.getBusinessImages(businessInfo))
 		).then((results) => results.reduce((acc, value) => acc.concat(value), []));
 	}
