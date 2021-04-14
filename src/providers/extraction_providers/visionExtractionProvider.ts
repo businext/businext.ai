04f222be0a1d53d71344789c3943431b2dea783d
Joshua Cheng <josh510cheng@gmail.com>
2021-04-14 18:06:32 -0400

    Add basic functionality

--- src/providers/extraction_providers/visionExtractionProvider.ts
+++ src/providers/extraction_providers/visionExtractionProvider.ts
@@ -1,11 +1,45 @@
-import { ExtractionProvider } from './extractionProvider'
-import { Image, ExtractedImage } from '../../models/data_models/image'
+import { ExtractionProvider } from './extractionProvider';
+import { Image, ExtractedImage } from '../../models/data_models/image';
+import { AssignedLabel, DetectedObject } from '../../models/data_models/extraction';
 
 import vision = require('@google-cloud/vision');
-const client = new vision.ImageAnnotatorClient();
 
 export class VisionExtractionProvider implements ExtractionProvider {
-    public extract(images: Array<Image>): Array<ExtractedImage> {
-        // TODO: implement with Google Vision
-    }
+	// constructor
+
+	// helper function for extracting one image at a time
+	protected async extractSingleImage(image: Image): Promise<ExtractedImage> {
+		const client = new vision.ImageAnnotatorClient();
+		const request = {
+			image: { source: { imageUri: '' } }, // TODO: add image url
+			features: [{ type: 'LABEL_DETECTION' }, { type: 'OBJECT_LOCALIZATION' }],
+		};
+		const [results] = await client.annotateImage(request);
+		const labels = results.labelAnnotations!;
+		const objects = results.localizedObjectAnnotations!;
+
+		// TODO: put each label into the assigned_labels
+		const assignedLabels: Array<AssignedLabel> = [];
+		// TODO: put each object detected into the detected_objects
+		const detectedObjects: Array<DetectedObject> = [];
+
+		const extracted: ExtractedImage = {
+			origin: {
+				source: image.source,
+				provider: image.provider,
+			},
+			extractions: {
+				assigned_labels: assignedLabels,
+				detected_objects: detectedObjects,
+			},
+		};
+
+		return extracted;
+	}
+
+	public extract(images: Array<Image>): Array<ExtractedImage> {
+		let extractedImages: Array<ExtractedImage> = [];
+		// TODO: run extractSingleImage on each image
+		return extractedImages;
+	}
 }
