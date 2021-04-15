a57fb66473e88d344692b23d030042abcbbbc3d0
Benny Lam <bennylam6@gmail.com>
2021-04-15 16:50:38 -0400

    Apply suggestions from code review
    Co-authored-by: Justin Xu <xu.justin.j@gmail.com>

--- src/providers/data_providers/mockBusinessImageProvider.ts
+++ src/providers/data_providers/mockBusinessImageProvider.ts
@@ -2,7 +2,7 @@ import { BaseBusinessImageProvider } from './baseBusinessImageProvider';
 import { Image, ImageProviderName } from '../../models/data_models/image';
 
 export class MockBusinessImageProvider extends BaseBusinessImageProvider {
-	public async getBusinessImages(): Promise<Array<Image>> {
+	public async getBusinessImages(businessInfo: BusinessInfoInput, geocode: Geocode): Promise<Array<Image>> {
 		return [
 			{
 				source: 'https://live.staticflickr.com/5475/11104337814_0ff2f27b74_b.jpg',
